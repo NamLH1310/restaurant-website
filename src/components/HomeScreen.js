7347e9a6bf9d35f298e05430a0df72debe927561
unknown <vietbdf000@gmail.com>
2021-11-03 21:38:51 +0700

    Add HomeScreen

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "tailwindcss/tailwind.css";
+
+function HomeScreen() {
+  return <div class="container mx-auto text-4xl">Hello World</div>;
+}
+
+export default HomeScreen;
