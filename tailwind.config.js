8d19aa824049994073d4a121741db3fee6c4eb50
hung.vinh.3591 <hungkinhngac2908@gmail.com>
2021-11-05 00:14:32 +0700

    Add NavigationBar and LoginForm

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primarycolor: "#83C75D",
+        primarybold:"#6ca34d",
+      }
+    },
+  },
+  fontFamily:{
+    display:["Nunito","san-serif"],
   },
   variants: {
     extend: {},
